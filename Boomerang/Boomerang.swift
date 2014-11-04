3eaf90995b9ef4504faff46705d91c54b340a3da
Smuurfisar <harrysson.olof@gmail.com>
2014-11-04 15:56:06 +0100

    första lite fungerande version

--- Boomerang/Boomerang.swift
+++ Boomerang/Boomerang.swift
@@ -5,5 +5,105 @@
 //  Created by Olof Harrysson on 2014-10-18.
 //  Copyright (c) 2014 Olof Harrysson. All rights reserved.
 //
+//println("\(length)")
+
 
 import Foundation
+import SpriteKit
+import UIKit
+
+class Boomerang: SKSpriteNode {
+
+    let POINTS_PER_SEC: CGFloat = 80.0
+    var wayPoints: [CGPoint] = []
+    var velocity = CGPoint(x: 0, y: 0)
+    var rotation: CGFloat = (CGFloat)(M_PI / 18) // Animation
+    var ySpeed: CGFloat!
+    var startDir: CGPoint!
+    var maxDis: CGFloat!
+    var length: CGFloat = 0.0
+    var isAlive: Bool = true
+    
+    init(imageNamed name: String!, wayPoints: [CGPoint]){
+        let boomSize: CGSize = CGSize(width: 46.0, height: 37.0)
+        let boomTex: SKTexture = SKTexture(imageNamed: "boomerang")
+        let boomNode = SKSpriteNode(texture: boomTex, size: boomSize)
+        super.init(texture: boomTex, color: nil, size:boomSize)
+        self.wayPoints = wayPoints
+        physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2.0)
+        physicsBody!.categoryBitMask = ColliderType.Boomerang.toRaw()
+        physicsBody!.contactTestBitMask = ColliderType.Obstacle.toRaw() | ColliderType.Goal.toRaw()
+        physicsBody!.collisionBitMask = 0
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func addMovingPoint(point: CGPoint) {
+        wayPoints.append(point)
+    }
+    
+    func setCurve () -> CGPathRef {
+        var boomPath = CGPathCreateMutable()
+        var beziPath = UIBezierPath()
+        
+        if wayPoints.count > 1 {
+            var tempWayPoints: [CGPoint] = []
+
+            var boomStartx : CGFloat = 150.0
+            var boomStarty : CGFloat = 140.0
+            CGPathMoveToPoint(boomPath!, nil, boomStartx, boomStarty)
+            let startPoint =  CGPoint(x: boomStartx, y:boomStarty)
+            beziPath.moveToPoint(startPoint)
+            beziPath.miterLimit = 30
+            
+            //offset so boomerang spawns where intended
+            var xOffset: CGFloat = boomStartx - wayPoints[0].x
+            var yOffset: CGFloat = boomStarty - wayPoints[0].y
+
+            
+            for wayP in wayPoints {
+                let point = CGPoint(x: wayP.x + xOffset, y: wayP.y + yOffset)
+                beziPath.addLineToPoint(point)
+            }
+            
+            let smoothBoomPath = beziPath.CGPath
+
+            return smoothBoomPath
+        }
+        return boomPath
+    }
+    
+    func setCurve1 () -> CGPathRef {
+        var boomPath = CGPathCreateMutable()
+        var beziPath = UIBezierPath()
+        
+        if wayPoints.count > 1 {
+            var tempWayPoints: [CGPoint] = []
+            
+            var boomStartx : CGFloat = 150.0
+            var boomStarty : CGFloat = 140.0
+            CGPathMoveToPoint(boomPath!, nil, boomStartx, boomStarty)
+            let startPoint =  CGPoint(x: boomStartx, y:boomStarty)
+            beziPath.moveToPoint(startPoint)
+            beziPath.miterLimit = 30 // Behövs?
+            
+            //offset so boomerang spawns where intended
+            var xOffset: CGFloat = boomStartx - wayPoints[0].x
+            var yOffset: CGFloat = boomStarty - wayPoints[0].y
+            
+            
+            for wayP in wayPoints {
+                let point = CGPoint(x: wayP.x + xOffset, y: wayP.y + yOffset)
+                beziPath.addLineToPoint(point)
+            }
+            
+            let smoothBoomPath = beziPath.CGPath
+            
+            return smoothBoomPath
+        }
+        return boomPath
+    }
+
+}
--- Boomerang/GameScene.swift
+++ Boomerang/GameScene.swift
@@ -7,39 +7,173 @@
 //
 
 import SpriteKit
+import AVFoundation
 
-class GameScene: SKScene {
+var backgroundMusicPlayer: AVAudioPlayer!
+
+enum ColliderType: UInt32 {
+    case Boomerang = 1
+    case Obstacle = 2
+    case Goal = 3
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate{
+    
+    var lastUpdateTime: NSTimeInterval = 0.0
+    var dt: NSTimeInterval = 0.0
+    var movingBoom: Boomerang?
+    var boomToThrow: Boomerang!
+    var wayPoints: [CGPoint] = []
+    let deadSound: SKAction = SKAction.playSoundFileNamed("deathrattle.wav", waitForCompletion: false)
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        physicsWorld.gravity = CGVectorMake(0.0, 0.0)
+        physicsWorld.contactDelegate = self
+        backgroundColor = SKColor.whiteColor()
+        loadLevel()
+        
+    }
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func throwBoom() {
+        if wayPoints.count > 1 {
+            let boomNode = Boomerang(imageNamed: "boomerang", wayPoints: wayPoints)
+
+
+            boomNode.name = "boom"
+            boomNode.zPosition = 2
+            addChild(boomNode)
+            
+            let boompath = boomNode.setCurve()
+            let moveAction = SKAction.followPath(boompath, asOffset: true, orientToPath: false, duration: 2.0)
+            boomNode.runAction(SKAction.sequence([ moveAction,
+                SKAction.fadeAlphaTo(0.0, duration: 0.3),
+                SKAction.removeFromParent()]))
+        }
+
+    }
+
+    
+    func addMovingPoint(point: CGPoint) {
+        wayPoints.append(point)
+    }
+
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+  
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+        let location = touches.anyObject()!.locationInNode(self)
+        addMovingPoint(location)
+
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let location = touches.anyObject()!.locationInNode(scene)
+        addMovingPoint(location)
+
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        throwBoom()
+        wayPoints = []
+    }
+    
+    
+
+    override func update(currentTime: CFTimeInterval) {
+        dt = currentTime - lastUpdateTime
+        lastUpdateTime = currentTime
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+        var kangNode = contact.bodyA.node;
+        var boomNode = contact.bodyB.node;
+        
+        if(kangNode!.name != "kang"){
+            let tempNode = boomNode
+            kangNode = boomNode
+            boomNode = tempNode
+        }
+        let kang = kangNode as SKSpriteNode
+        let boom = boomNode as Boomerang
+        
+
+        let collision = kangNode!.physicsBody!.categoryBitMask | boomNode!.physicsBody!.categoryBitMask;
+        
+
+        if collision == ColliderType.Boomerang.toRaw() | ColliderType.Goal.toRaw() {
+            let kangPos = kang.position
+            kang.removeFromParent()
+            boom.physicsBody = nil
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let deadKangNode = childNodeWithName("deadkang")
+            let deadKang = deadKangNode as SKSpriteNode
+            let newPos = CGPoint(x: kangPos.x - 5, y: kangPos.y - 10)
+            deadKang.position = newPos
+            deadKang.hidden = false
+            runAction(deadSound)
+            deadKang.runAction(SKAction.fadeAlphaTo(1.0, duration: 0.0))
+            deadKang.runAction(SKAction.fadeAlphaTo(0.0, duration: 1.0))
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            spawnKang()
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        } else if collision == ColliderType.Boomerang.toRaw() | ColliderType.Obstacle.toRaw() {
+            println("FAIL :[")
+        }else if collision == ColliderType.Boomerang.toRaw() | ColliderType.Boomerang.toRaw() {
+                println("Boom krock")
+        } else {
+            NSLog("Error: Unknown collision category \(collision)");
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    func loadLevel() {
+        
+        spawnKang()
+    
+        let deadkangSize: CGSize = CGSize(width: 50.0, height: 60.0)
+        let deadKangTex: SKTexture = SKTexture(imageNamed: "deadkangaroo")
+        let deadKangNode = SKSpriteNode(texture: deadKangTex, size: deadkangSize)
+        deadKangNode.name = "deadkang"
+        deadKangNode.hidden = true
+        deadKangNode.zRotation = CGFloat(M_PI / 2)
+        addChild(deadKangNode)
+        
+        let playerSize: CGSize = CGSize(width: 96.6, height: 150)
+        let playerTex: SKTexture = SKTexture(imageNamed: "banksy")
+        let playerNode = SKSpriteNode(texture: playerTex, size: playerSize)
+        playerNode.name = "player"
+        playerNode.position = CGPoint(x: size.width * 0.5, y: 100)
+        addChild(playerNode)
+        
     }
+    
+    func spawnKang () {
+        let kangSize: CGSize = CGSize(width: 50.0, height: 60.0)
+        let kangTex: SKTexture = SKTexture(imageNamed: "kangaroo")
+        let kangNode = SKSpriteNode(texture: kangTex, size: kangSize)
+        
+        kangNode.name = "kang"
+
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let xSpawnPos: Int = Int(arc4random_uniform(UInt32(screenWidth+1 - kangSize.width))) + Int(kangSize.width / 2)
+        
+        kangNode.position = CGPoint(x: xSpawnPos, y: 550)
+        kangNode.zPosition = 1
+        
+        kangNode.physicsBody = SKPhysicsBody(rectangleOfSize: kangNode.size)
+        kangNode.physicsBody!.categoryBitMask = ColliderType.Goal.toRaw()
+        kangNode.physicsBody!.dynamic = false
+        
+        addChild(kangNode)
+    }
+    
+    
 }
